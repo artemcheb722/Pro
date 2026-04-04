e6cb834b759fb80856365d26c2abeda07002b700
Artem <artem.chebanyuk@gmail.com@email.com>
2026-04-04 19:37:06 +0300

    7 - task 4

---
+++ task_4.py
@@ -0,0 +1,12 @@
+def check(number):
+    return "Парне" if number % 2 == 0 else "Не парне"
+num = int(input("Введіть число: "))
+print(check(num))
+
+
+
+def check1(numbers):
+ return {i for i in numbers if i % 2 == 0}
+
+nums = set(map(int, input("Введіть числа: ").split()))
+print(check1(nums))
